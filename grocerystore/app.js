cff21ea3a049f5d8ed488e9a9fbb7a565565ae4e
Annette O'Reilly <acella4@gmail.com>
2019-11-26 09:27:47 -0700

    computer broken push

---
+++ grocerystore/app.js
@@ -0,0 +1,14 @@
+var shopper = {
+    name: "Rhea",
+    age: 26,
+    isBuyingClothes: true,
+    groceryCart: ["shoes", "handbag", "makeup", "sweater"],
+    fullName: function(){
+        return this.firstName + " " + this.LastName
+    }
+}
+console.log(shopper.age)
+
+
+
+
